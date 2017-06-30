56365914bbc45be91f78f4744adce92320b15c32
kee <keerotic@gmail.com>
2017-06-30 09:14:04 +0700

    setup PWA

---
+++ sw.js
@@ -0,0 +1,46 @@
+var dataCacheName = 'pwagame';
+var cachedName = 'pwagame';
+var base = '';
+var filesToCache = [
+  base+'/',
+  base+'/index.html',
+  base+'/css/style.css',
+  base+'/js/game.js',
+];
+
+self.addEventListener('install', e => {
+  console.log('[ServiceWorker] Installed')
+  e.waitUntil(preCache())
+})
+
+self.addEventListener('fetch', e => {
+  console.log('[ServiceWorker] Serving the asset.')
+  e.respondWith(fromNetwork(e.request, 1000).catch(() => {
+    return fromCache(e.request)
+  }))
+})
+
+const preCache = () => {
+  caches.open(cachedName).then((cache) => {
+    console.log('[ServiceWorker] Caching app shell')
+    return cache.addAll(filesToCache)
+  })
+}
+
+const fromNetwork = (req, timeout) => {
+  return new Promise((resolve, reject) => {
+    let timeoutId = setTimeout(reject, timeout)
+    fetch(req).then((res) => {
+      clearTimeout(timeoutId)
+      resolve(res)
+    }, reject)
+  })
+}
+
+const fromCache = (req) => {
+  return caches.open(cachedName).then(cache => {
+    return cache.match(req).then(matching => {
+      return matching || Promise.reject(new Error('no-match'))
+    })
+  })
+}
