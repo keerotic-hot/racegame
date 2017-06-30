d359dddc6aab02b30f101792f567a55a1cdd639a
kee <keerotic@gmail.com>
2017-06-30 15:53:36 +0700

    create box

--- js/game/mainmenu.js
+++ js/game/mainmenu.js
@@ -5,17 +5,26 @@ function MainMenu(renderer){
 	var _renderer = renderer || new THREE.WebGLRenderer();
 
 
-	camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,1,4000);
-	camera.position.set(0,0,0);
-	controls = new THREE.DeviceOrientationControls(camera);
+	_camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,1,4000);
+	_camera.position.set(100,100,100);
+	_camera.up = new THREE.Vector3(0,0,1);
+	_camera.lookAt(new THREE.Vector3(0,0,0));
+	//controls = new THREE.DeviceOrientationControls(_camera);
+
+	var geo = new THREE.BoxGeometry(10,10,10,1,1,1);
+	var mat = new THREE.MeshBasicMaterial({color:0xffff00,wireframe: true });
+	var mesh = new THREE.Mesh(geo,mat);
+	_scene.add(mesh);
 
 
 	_this.onWindowResize = function(){
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();	
+		_camera.aspect = window.innerWidth / window.innerHeight;
+		_camera.updateProjectionMatrix();	
 	}
 
 	_this.render = function(){
+		mesh.rotateX(Math.PI/180);
+		mesh.rotateY(2*Math.PI/180);
 		_renderer.render(_scene,_camera);
 	}
 }
